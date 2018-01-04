c298d7857aedfb1adf25223b1125d1fa6667becb
Amomum <amomum@yandex.ru>
2018-01-04 21:39:29 +0300

    Added optional argument, bumped version number, added support for .constdata.
    Still something doesn't add up.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::io::BufWriter;
 
+#[macro_use]
 extern crate clap;
 use clap::App;
 use clap::Arg;
@@ -14,17 +15,17 @@ use regex::Regex;
 struct LinkerSymbol {
     name: String,
     object_file: String,
-    size: u32, 
-    //adr: u32,
+    size: u32,
+    orig: String
 }
 
 impl LinkerSymbol {
     pub fn new() -> LinkerSymbol {
         LinkerSymbol {
             name: String::new(),
-            //adr: 0,
             object_file: String::new(),
             size: 0,
+            orig: String::new()
         }
     }
 }
@@ -32,18 +33,25 @@ impl LinkerSymbol {
 fn main() {
 
     let args = App::new("armlink map chartist")
-        .version("0.1")
-        .author("amomum")
+        .version(crate_version!())
         .about("A simple parser of armlink map file. \
-        Primary usage is code-size optimization - you will be able to find out what functions should take the blame.")
+        Primary usage is code-size optimization - you will be able to find out what code should take the blame. \
+        Output is TAB-separated file because comma and space can be in the symbol names. \
+        \nBEWARE: It's not very accurate! The total numbers do not always add up with armlink total info.")
         .arg( Arg::with_name("map file")
-        .index(1)
-        .required(true)
-        .help("Input linker map file"))
+            .index(1)
+            .required(true)
+            .help("Input linker map file") )
+        .arg( Arg::with_name("output file name")
+            .index(2)
+            .required( false )
+            .default_value("result.txt")
+            .help( "Output text file name")
+        )
         .get_matches();
 
-    let file_name = args.value_of("map file").unwrap();
-    let f = File::open(file_name).unwrap();
+    let map_file_name = args.value_of("map file").unwrap();
+    let f = File::open(map_file_name).unwrap();
 
     let reader = BufReader::new(&f);
 
@@ -56,33 +64,34 @@ fn main() {
 
     let mut linker_symbols: Vec<Box<LinkerSymbol>> = Vec::new();
 
-    //let regex_addr = Regex::new(r"\s0[xX][0-9a-fA-F]+").unwrap();
     let regex_func_name = Regex::new(r".*?(?:\s0x)").unwrap();
     let regex_size = Regex::new(r"\s\d+\s").unwrap();
     let regex_obj_file = Regex::new(r"\w+[.][o]").unwrap();
 
     let line_iter = line_iter.enumerate();
 
+    let mut code_size = 0;
+    let mut const_data_size = 0;
+
     for (_, line) in line_iter {
         // end of section
 
         let line = line.unwrap();
 
-        //println!("{} : {}", i, line.as_str());
-
-        if line.contains("==============================================================================") { 
+        if line.contains("==============================================================================") {
             break;
         }
 
-        if !line.contains("Thumb Code") {
+        if !line.contains("Thumb Code") && !line.contains( ".constdata"){
             continue;
         }
 
-        //println!("----- Line accepted");
-
         let mut symbol = Box::new(LinkerSymbol::new());
         let line_str = line.as_str();
-        // unfortunately I couldn't craft ideal regexp so I have to remove the traling "0x" like that
+
+        symbol.orig = line_str.to_string();
+
+        // unfortunately I couldn't craft ideal regexp so I have to remove the trailing "0x" like that
         symbol.name = regex_func_name
             .captures(line_str)
             .unwrap()
@@ -110,15 +119,12 @@ fn main() {
             .as_str()
             .to_string();
 
-        // let t = regex_addr
-        //     .captures(line_str)
-        //     .unwrap()
-        //     .get(0)
-        //     .unwrap()
-        //     .as_str()
-        //     .trim();
-
-        // symbol.adr = u32::from_str_radix(&t[2..], 16).unwrap();
+        if line.contains("Thumb Code" ) {
+            code_size += symbol.size;
+        }
+        else {
+            const_data_size += symbol.size;
+        }
 
         linker_symbols.push(symbol);
     }
@@ -127,13 +133,13 @@ fn main() {
     linker_symbols.sort_by(|a, b| a.size.cmp(&b.size));
     linker_symbols.reverse();
 
-    println!("Code size: {}",
-             linker_symbols
-                 .iter()
-                 .fold(0u32, |sum, symbol| sum + symbol.size));
+    println!("Code size: {}, const data size {}", code_size, const_data_size );
 
     // write them to file with TAB as a separator since both comma and space could be inside function names
-    let file = File::create("result.txt").unwrap();
+
+    let output_filename = args.value_of("output file name").unwrap();
+
+    let file = File::create(output_filename).unwrap();
     let mut writer = BufWriter::new(&file);
 
     for symbol in linker_symbols {
